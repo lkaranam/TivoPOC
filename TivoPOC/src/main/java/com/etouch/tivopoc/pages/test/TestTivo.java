35ce583c71f957114706bd0822512784cc2a983a
Lavanya <lkaranam@etouch.net>
2015-05-12 14:39:05 -0700

    updated files

--- TivoPOC/src/main/java/com/etouch/tivopoc/pages/test/TestTivo.java
+++ TivoPOC/src/main/java/com/etouch/tivopoc/pages/test/TestTivo.java
@@ -1,7 +1,7 @@
 package com.etouch.tivopoc.pages.test;
 
 import org.apache.commons.logging.Log;
-
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -10,6 +10,7 @@ import com.etouch.taf.core.datamanager.excel.TafExcelDataProvider;
 import com.etouch.taf.core.datamanager.excel.TestParameters;
 import com.etouch.taf.core.datamanager.excel.annotations.IExcelDataFiles;
 import com.etouch.taf.core.datamanager.excel.annotations.ITafExcelDataProviderInputs;
+import com.etouch.taf.tools.rally.VideoRecorder;
 import com.etouch.taf.util.CommonUtil;
 import com.etouch.taf.util.LogUtil;
 import com.etouch.taf.util.SoftAssertor;
@@ -34,8 +35,11 @@ public class TestTivo extends BaseTest {
 
 	/** The web page. */
 	private WebPage webPage;
+	
+	private WebDriver driver;
 
 	private TivoPages tivoPages;
+	VideoRecorder videoRecorder = null;
 
 	// required for jira
 	/** The url3. */
@@ -72,6 +76,7 @@ public class TestTivo extends BaseTest {
 	String WORKSPACE_ID = TestBedManager.INSTANCE.getDefectConfig()
 			.getWorkspaceId();
 
+
 	/**
 	 * Prepare before class.
 	 * 
@@ -86,31 +91,53 @@ public class TestTivo extends BaseTest {
 			webPage = new WebPage();
 			currentUrl = "http://www.rcn2go.com/tve/start";
 			tivoPages = new TivoPages(currentUrl, webPage);
-
+			//videoRecorder = new VideoRecorder("/Users/malathikosaraju/Documents/etouchWorkspace/TivoPOC/src/test/resources/videos/");
+			
+			//changes for running in jenkins
+			//reading video file path from Chrome job and Firefox job
+			driver = webPage.getDriver();
+			if(driver.toString().startsWith("ChromeDriver:")) {
+				videoRecorder = new VideoRecorder("/Users/rpandey/workspace/TivoPOC-Chrome/TivoPOC/src/test/resources/videos/");
+			}else if (driver.toString().startsWith("FirefoxDriver:")) {
+				videoRecorder = new VideoRecorder("/Users/rpandey/workspace/TivoPOC-Firefox/TivoPOC/src/test/resources/videos/");
+			}
+			
+			
 		}catch (InterruptedException e) {
 			SoftAssertor.addVerificationFailure(e.getMessage());
 			//e.printStackTrace();
 			log.error(e.getMessage());
 		}
+		
 	}
 	@Test(dataProvider = "tafDataProvider", dataProviderClass = TafExcelDataProvider.class, priority = 1)
 	@ITafExcelDataProviderInputs(excelFile = "file1", excelsheet = "TestTivoPOC_Defects", dataKey = "SignIn")
 	public void testSignIn(TestParameters inputs) {
 		CommonUtil.sop("Starting testSignIn");
-	
+		
 		String username = "DCtesting1";
 		String pwd = "testing123tivo";
-		try { 
-			tivoPages.signIn(username, pwd);
 		
+		try { 
+			videoRecorder.startRecording();
+			CommonUtil.sop("Recording feature on");
+
+			tivoPages.signIn(username, pwd);
+			
 		} catch (Exception e) {
 			SoftAssertor.addVerificationFailure(e.getMessage());
-			//e.printStackTrace();
 			log.error(e.getMessage());
 		}
 		finally
 		{		
-			logDefect(inputs);
+			String errMsg = SoftAssertor.readErrorsForTest();
+			if(errMsg!=null && errMsg=="") {
+				videoRecorder.stopRecording();
+				videoRecorder.deleteRecording();
+				CommonUtil.sop("Stopped Recording at the end");
+			}else {
+				logDefect(inputs,errMsg);
+			}
 			SoftAssertor.displayErrors();
 		}
 		
@@ -121,16 +148,25 @@ public class TestTivo extends BaseTest {
 		CommonUtil.sop("Starting testAddAShow");
 	
 		try {
+			videoRecorder.startRecording();
+			CommonUtil.sop("Recording feature on");
+			
 			tivoPages.addAShow();
 
 		} catch (Exception e) {
 			SoftAssertor.addVerificationFailure(e.getMessage());
-			//e.printStackTrace();
 			log.error(e.getMessage());
 		}
 		finally
-		{			
-			logDefect(inputs);
+		{		
+			String errMsg = SoftAssertor.readErrorsForTest();
+			if( errMsg!=null && errMsg=="") {
+				videoRecorder.stopRecording();
+				videoRecorder.deleteRecording();
+				CommonUtil.sop("Stopped Recording at the end");
+			}else {
+				logDefect(inputs,errMsg);
+			}
 			SoftAssertor.displayErrors();
 		}
 	
@@ -141,16 +177,25 @@ public class TestTivo extends BaseTest {
 		CommonUtil.sop("Starting testDeleteAShow");
 
 		try {
+			videoRecorder.startRecording();
+			CommonUtil.sop("Recording feature on");
+			
 			tivoPages.deleteAShow();
 			
 		} catch (Exception e) {
 			SoftAssertor.addVerificationFailure(e.getMessage());
-			//e.printStackTrace();
 			log.error(e.getMessage());
 		}
 		finally
-		{			
-			logDefect(inputs);
+		{		
+			String errMsg = SoftAssertor.readErrorsForTest();
+			if( errMsg!=null && errMsg=="") {
+				videoRecorder.stopRecording();
+				videoRecorder.deleteRecording();
+				CommonUtil.sop("Stopped Recording at the end");
+			}else {
+				logDefect(inputs,errMsg);
+			}
 			SoftAssertor.displayErrors();
 		}
 		
@@ -162,18 +207,28 @@ public class TestTivo extends BaseTest {
 
 		String showTitle = "House of Cards";
 		try {
+			videoRecorder.startRecording();
+			CommonUtil.sop("Recording feature on");
+			
 			tivoPages.searchForAShow(showTitle);
 
 		} catch (Exception e) {
 				
 			SoftAssertor.addVerificationFailure(e.getMessage());
-			//e.printStackTrace();
 			log.error(e.getMessage());
 		}
 		finally
-		{			
-			logDefect(inputs);
-			SoftAssertor.displayErrors();
+		{	
+			String errMsg = SoftAssertor.readErrorsForTest();
+			if( errMsg!=null && errMsg=="") {
+				videoRecorder.stopRecording();
+				videoRecorder.deleteRecording();
+				CommonUtil.sop("Stopped Recording at the end");
+			}else {
+				
+				logDefect(inputs,errMsg);
+			}
+			SoftAssertor.displayErrors();	
 		}
 	}
 		
@@ -183,36 +238,43 @@ public class TestTivo extends BaseTest {
 		CommonUtil.sop("Starting testsignOut");
 
 		try {
+			videoRecorder.startRecording();
+			CommonUtil.sop("Recording feature on");
+			
 			tivoPages.signOut();
 			
 		} catch (Exception e) {
 			SoftAssertor.addVerificationFailure(e.getMessage());
-			//e.printStackTrace();
 			log.error(e.getMessage());
 		}
 		finally
 		{		
-			
-			logDefect(inputs);
+			String errMsg = SoftAssertor.readErrorsForTest();
+			if( errMsg!=null && errMsg=="") {
+				videoRecorder.stopRecording();
+				videoRecorder.deleteRecording();
+				CommonUtil.sop("Stopped Recording at the end");
+				
+			}else {
+				logDefect(inputs,errMsg);
+			}
 			SoftAssertor.displayErrors();
 		}
 		
 	}
 	
-	public void logDefect(TestParameters inputs)
+	public void logDefect(TestParameters inputs, String errorMsg)
 	{
 		String SEARCH_DEFECT_NAME = inputs.getParamMap().get("SearchDefectName");	
 		String SEARCH_TEST_ID =  inputs.getParamMap().get("RallyTestCaseID");
 		String DEFECT_SEVERITY =  inputs.getParamMap().get("DefectSeverity");
-		String DEFECT_NOTES =  inputs.getParamMap().get("DefectNotes");
+		String DEFECT_NOTES =  inputs.getParamMap().get("DefectNotes") + " : " + "Video Link: "+ videoRecorder.getVideoLink();
 		String attachmentPath = ".//src//test//resources//screenshots";
-		String errMsg = SoftAssertor.readErrorsForTest();
 		
-		if(errMsg!=null && errMsg.length()>0)
-		{
+			videoRecorder.stopRecording();
+			CommonUtil.sop("Stopped Recording at the end");
 			tivoPages.getScreenshot();
-			tivoPages.logAndCreateADefect(TestBedManager.INSTANCE.getDefect(),DEFECT_PROP,SEARCH_TEST_ID, WORKSPACE_ID, PROJECT_ID,  STORY_ID, SEARCH_DEFECT_NAME, DEFECT_SEVERITY,DEFECT_OWNER,DEFECT_NOTES, errMsg, attachmentPath);
-		}
+			tivoPages.logAndCreateADefect(TestBedManager.INSTANCE.getDefect(),DEFECT_PROP,SEARCH_TEST_ID, WORKSPACE_ID, PROJECT_ID,  STORY_ID, SEARCH_DEFECT_NAME, DEFECT_SEVERITY,DEFECT_OWNER,DEFECT_NOTES, errorMsg, attachmentPath);
 		
 	}
 	
